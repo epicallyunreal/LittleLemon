a1cc42365a0024fcebbcad973a706ea7818803e2
epicallyunreal <prabhat9819@gmail.com>
2026-07-31 19:53:24 +0530

    booking apis using routers

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('index2/', views.index2, name='index2'),
+    path('menu/', views.MenuItemView.as_view(), name='menu-items'),
+    path('menu/<int:pk>/', views.SingleMenuItemView.as_view(), name='single-menu-item'),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+import rest_framework.generics
+import rest_framework.viewsets
+
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
 
 # Create your views here.
 def index(request):
@@ -6,3 +11,15 @@ def index(request):
 
 def index2(request):
     return render(request, 'index2.html', {})
+
+class MenuItemView(rest_framework.generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(rest_framework.generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(rest_framework.viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
